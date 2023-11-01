69ac15e1fe2d03f40cf2f8fd7379ce69f42f8b11
Meshari AlMutairi <37935866+Mesharikw@users.noreply.github.com>
2023-11-01 06:57:46 +0300

    Update get_shift.py
    enhancement from function to class based view

--- get_shift.py
+++ get_shift.py
@@ -1,58 +1,30 @@
 import datetime
 
 
-def get_shfit(date=None, shift=None):
-
-    # date:     date in string
-    # shift:    0 - for day shift, 1 - for Night shift, 2 - for 1st off, 3 - for 2nd off
-
-    shifts = {
-        'day': {
-            0: 'A',
-            25: 'B',
-            5: 'C',
-            75: 'D'
-        },
-        'night': {
-            25: 'A',
-            5: 'B',
-            75: 'C',
-            0: 'D'
-        },
-        'first_off': {
-            5: 'A',
-            75: 'B',
-            0: 'C',
-            25: 'D'
-        },
-        'second_off': {
-            75: 'A',
-            0: 'B',
-            25: 'C',
-            5: 'D'
+class ShiftScheduler:
+    def __init__(self, date=None, time=None):
+        self.EPOCH_DATE = datetime.date(1970, 1, 1)
+        self.shift_names = ['a', 'b', 'c', 'd']
+        self.shift_schedule = {
+            'day': [0, 25, 50, 75],
+            'night': [25, 50, 75, 0],
+            'first_off': [50, 75, 0, 25],
+            'second_off': [75, 0, 25, 50]
         }
-    }
+        self.date = datetime.datetime.today().date() if date is None else date
+        if time is not None:
+            self.date = datetime.datetime(self.date.year, self.date.month, self.date.day, time.hour, time.minute).date()
 
-    today = datetime.datetime.now().date()
-    date_beings = datetime.date(1983, 1, 1)
-    if not date:
-        date = today
-    elif type(date) is str:
-        date = datetime.datetime.strptime(date, "%d/%m/%Y").date()
-    else:
-        raise Exception("Date should be string or empty!")
-    diff_dates = (date - date_beings).days
-    div_dates = diff_dates / 4
-    map_date = int((div_dates - int(div_dates)) * 100)
-    if shift == 0:
-        return shifts['day'].get(map_date)
-    elif shift == 1:
-        return shifts['night'].get(map_date)
-    elif shift == 2:
-        return shifts['first_off'].get(map_date)
-    elif shift == 3:
-        return shifts['second_off'].get(map_date)
-    elif not shift:
-        return {'day': shifts['day'].get(map_date), 'night': shifts['night'].get(map_date),
-                '1st-off': shifts['first_off'].get(map_date), '2nd-off': shifts['second_off'].get(map_date)}
+    def get_shift_index(self, date):
+        days_since_epoch = (date - self.EPOCH_DATE).days
+        return int((days_since_epoch / 4 - int(days_since_epoch / 4)) * 100)
 
+    def get_shift(self, shift_type, date=None):
+        date = self.date if date is None else date
+        index = self.get_shift_index(date)
+        shift_position = self.shift_schedule[shift_type].index(index)
+        return self.shift_names[shift_position]
+
+    def get_shifts(self, date=None):
+        date = self.date if date is None else date
+        return {shift_type: self.get_shift(shift_type, date) for shift_type in self.shift_schedule}
